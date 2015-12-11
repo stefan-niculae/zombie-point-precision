52d5ec1db971c8e142dce10ff50a984df7b74367
zsefv009 <botolanvladalexandru@gmail.com>
2015-12-12 00:07:07 +0200

    coins1

---
+++ ZPP/Assets/MoneyScripts/MoneyGameControler.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoneyGameControler : MonoBehaviour {
+
+    public GameObject coin;
+    public Camera cam;
+    
+    
+    private float maxWidth;
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float coinWidth = coin.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - coinWidth;
+        StartCoroutine(SpawnCoin());
+        
+    }
+
+   
+
+    
+    IEnumerator SpawnCoin()
+    {
+        yield return new WaitForSeconds(1.0f);
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(
+                                                Random.Range(-maxWidth+4, maxWidth-1),
+                                                transform.position.y,
+                                                0.0f
+                                                );
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(coin, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(2.0f, 3.0f));
+        }
+        
+    }
+}
---
+++ ZPP/Assets/MoneyScripts/PalmMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PalmMovement : MonoBehaviour
+{
+
+    float speed = 5.0f;
+    KeyCode left = KeyCode.LeftArrow;
+    KeyCode right = KeyCode.RightArrow;
+
+    // Use this for initialization
+    void Start()
+    {
+        //transform.position = new Vector2(0, -3.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(left))
+        {
+            Move(-1);
+        }
+        else if (Input.GetKey(right))
+        {
+            Move(1);
+        }
+    }
+
+    void Move(int direction)
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position,
+                                               new Vector3(5f * direction, transform.position.y, transform.position.z),
+                                               speed * Time.deltaTime);
+
+    }
+
+}
