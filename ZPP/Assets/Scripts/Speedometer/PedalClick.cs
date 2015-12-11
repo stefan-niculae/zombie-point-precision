aca971c984d00c22cd911f8c137d2af239b62089
PandaZuZU <txhung08@gmail.com>
2015-12-11 23:09:47 +0200

    Final Commit
    final commit, just add end game

--- ZPP/Assets/Scripts/Speedometer/PedalClick.cs
+++ ZPP/Assets/Scripts/Speedometer/PedalClick.cs
@@ -5,11 +5,13 @@ public class PedalClick : MonoBehaviour {
 
     public Transform maxRotation;
     public Transform minRotation;
+    public Transform pedalDown, pedalUp, pedal;
+    public Transform footDown, footUp, foot;
 
     private readonly float ROTATION_SPEED = 100f;
     private readonly float ROTATION_ERROR = 0.01f;
 
-    private KeyCode accelration = KeyCode.Space;
+    private KeyCode acceleration = KeyCode.Space;
 
     private bool startGame;
 
@@ -22,7 +24,7 @@ public class PedalClick : MonoBehaviour {
 
     void Update()
     {
-        if (Input.GetKey(accelration))
+        if (Input.GetKey(acceleration))
         {
             if (transform.rotation.z + ROTATION_ERROR > maxRotation.rotation.z)
             {
@@ -44,11 +46,22 @@ public class PedalClick : MonoBehaviour {
     void Accelerate()
     {
         transform.Rotate(new Vector3(0f, 0f, -1f) * ROTATION_SPEED * Time.deltaTime);
+        if (foot.rotation.z - footDown.rotation.z < ROTATION_ERROR)
+        {
+            foot.Rotate(new Vector3(0f, 0f, 0.1f) * ROTATION_SPEED * Time.deltaTime);
+            pedal.Rotate(new Vector3(-0.1f, 0f, 0f) * ROTATION_SPEED * Time.deltaTime);
+        }
+
     }
 
     void Decelerate()
     {
         transform.Rotate(new Vector3(0f, 0f, 1f) * ROTATION_SPEED * Time.deltaTime);
+        if (foot.rotation.z - footUp.rotation.z > ROTATION_ERROR)
+        {
+            foot.Rotate(new Vector3(0f, 0f, -0.1f) * ROTATION_SPEED * Time.deltaTime);
+            pedal.Rotate(new Vector3(0.1f, 0f, 0f) * ROTATION_SPEED * Time.deltaTime);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
