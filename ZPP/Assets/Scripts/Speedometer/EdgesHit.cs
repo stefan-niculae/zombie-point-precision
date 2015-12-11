aee5544c44caec0305c6f8c93b17dc480307c028
PandaZuZU <txhung08@gmail.com>
2015-12-11 21:39:38 +0200

    Needle logics
    Needle starts moving, but trigger bug

---
+++ ZPP/Assets/Scripts/Speedometer/EdgesHit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgesHit : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hit");
+        if(other.gameObject.name == "needleEdge")
+        {
+            Debug.Log("NEEDLE");
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ ZPP/Assets/Scripts/Speedometer/PedalClick.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PedalClick : MonoBehaviour {
+
+    public Transform maxRotation;
+    public Transform minRotation;
+
+    private readonly float ROTATION_SPEED = 100f;
+
+    private KeyCode accelration = KeyCode.Space;
+
+    private bool startGame;
+
+    IEnumerator Start()
+    {
+        startGame = false;
+        yield return new WaitForSeconds(1.5f);
+        startGame = true;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(accelration))
+        {
+            Accelerate();
+        }
+        else
+        {
+            if (startGame)
+            {
+                Decelerate();
+            }
+        }
+    }
+
+    void Accelerate()
+    {
+        transform.Rotate(new Vector3(0f, 0f, -1f) * ROTATION_SPEED * Time.deltaTime);
+    }
+
+    void Decelerate()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 1f) * ROTATION_SPEED * Time.deltaTime);
+    }
+}
