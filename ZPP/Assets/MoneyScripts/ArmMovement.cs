5204f013620a73b6af4a8f34e6ea4e1ae6b58c1f
PandaZuZU <txhung08@gmail.com>
2015-12-12 05:34:07 +0200

    commit before final

--- ZPP/Assets/MoneyScripts/ArmMovement.cs
+++ ZPP/Assets/MoneyScripts/ArmMovement.cs
@@ -4,53 +4,15 @@ using System.Collections;
 public class ArmMovement : MonoBehaviour
 {
 
-    float speed = 5.0f;
+
     KeyCode left = KeyCode.LeftArrow;
     KeyCode right = KeyCode.RightArrow;
 
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(left))
-        {
-            Move(-0.01f);
-            Rotate(-0.01f);
-            Grow(-1.8f);
-        }
-        else if (Input.GetKey(right))
-        {
-            Move(0.01f);
-            Rotate(0.01f);
-            Grow(1.8f);
-        }
-    }
-
-    void Move(float direction)
-    {
-        transform.position = Vector3.MoveTowards(transform.position,
-                                               new Vector3(0.2f * direction, transform.position.y, transform.position.z),
-                                               speed * Time.deltaTime);
 
     }
-    void Rotate(float delta)
-    {
-        var rot = transform.rotation;
-        rot.z += delta * Time.deltaTime;
-        transform.rotation = rot;
 
-    }
-    void Grow(float Epsilon)
-    {
-        var lenght = transform.localScale;
-        lenght.x += Epsilon * Time.deltaTime;
-        transform.localScale = lenght;
-    }
 }
 
--- ZPP/Assets/MoneyScripts/PalmMovement.cs
+++ ZPP/Assets/MoneyScripts/PalmMovement.cs
@@ -4,9 +4,15 @@ using System.Collections;
 public class PalmMovement : MonoBehaviour
 {
 
-    float speed = 5.0f;
+    private readonly float MOVEMENT_SPEED = 5.0f;
+    private readonly float ROTATION_ERROR = 0.01f;
+    private readonly float ROTATION_SPEED = 300f;
+
     KeyCode left = KeyCode.LeftArrow;
     KeyCode right = KeyCode.RightArrow;
+
+    public GameObject handBottom;
+    public GameObject handTop;
 
     // Use this for initialization
     void Start()
@@ -31,8 +37,11 @@ public class PalmMovement : MonoBehaviour
     {
 
         transform.position = Vector3.MoveTowards(transform.position,
-                                               new Vector3(5f * direction, transform.position.y, transform.position.z),
-                                               speed * Time.deltaTime);
+                                               new Vector3(2f * direction, transform.position.y, transform.position.z),
+                                               MOVEMENT_SPEED * Time.deltaTime);
+
+        handBottom.transform.localScale -= new Vector3(direction  * -0.02f, 0f, 0f);
+        handTop.transform.Rotate(new Vector3(0f, 0f, direction * 0.1f) * Time.deltaTime * ROTATION_SPEED);
 
     }
 
