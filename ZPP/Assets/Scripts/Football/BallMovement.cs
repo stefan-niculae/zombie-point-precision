a8794fa0bfdca36b7aefc1b3155f619f8c989430
PandaZuZU <txhung08@gmail.com>
2015-12-12 04:26:11 +0200

    Football commit
    added foot animation

--- ZPP/Assets/Scripts/Football/BallMovement.cs
+++ ZPP/Assets/Scripts/Football/BallMovement.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class BallMovement : MonoBehaviour {
 
-    private Vector2 direction;
     private readonly float BALL_SPEED = 15f;
+    private readonly float BACKDISTANCE = 1f;
+
     private int randomPos;
+    private bool isHit;
+    private Vector2 direction;
+    private Vector2 upDirection = new Vector2(7f, 7f);
+    private Vector2 downDirection = new Vector2(7f, -7f);
+
+    private GameObject foot;
+
+    void Start()
+    {
+        foot = GameObject.Find("Foot");
+        isHit = false;
+    }
 
     public void SetDirection(Vector2 direction)
     {
@@ -15,13 +28,36 @@ public class BallMovement : MonoBehaviour {
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, direction, Time.deltaTime * BALL_SPEED);
+
+        if (isHit)
+        {
+            GetComponent<SpriteRenderer>().color -= new Color(0f,0f,0f,0.02f);
+        }
+
+        if(GetComponent<SpriteRenderer>().color.a <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
+
         if(other.gameObject.name == "right foot")
         {
-            Debug.Log("HIT");
+            isHit = true;
+            foot.GetComponent<ShootBall>().setHit(false);
+
+            if (direction.y >= 0)
+            {
+                direction = upDirection;
+            }
+            else
+            {
+                direction = downDirection;
+            }
+
         }
     }
+
 }
--- ZPP/Assets/Scripts/Football/ShootBall.cs
+++ ZPP/Assets/Scripts/Football/ShootBall.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class ShootBall : MonoBehaviour {
 
-    private readonly float ROTATION_SPEED = 0.01f;
+    private readonly float ROTATION_SPEED = 800f;
     private readonly float MOVE_SPEED = 15f;
+    private readonly float ROTATION_ERROR = 0.01f;
+    private bool isHitting;
+
     private Vector2 upPosition = new Vector2(-5f, 2f);
     private Vector2 downPosition = new Vector2(-5f, -0.5f);
     private Vector2 movingTowards;
@@ -12,9 +15,12 @@ public class ShootBall : MonoBehaviour {
     private KeyCode up = KeyCode.UpArrow;
     private KeyCode down = KeyCode.DownArrow;
 
+    public Transform defaultRotation, startRotation, endRotation;
+
     void Start()
     {
-        movingTowards = transform.position;
+        movingTowards = startRotation.position;
+        isHitting = true;
     }
 
     void Update()
@@ -30,16 +36,32 @@ public class ShootBall : MonoBehaviour {
 
         transform.position = Vector2.MoveTowards(transform.position, movingTowards, Time.deltaTime * MOVE_SPEED);
 
+        if(transform.rotation.z - startRotation.rotation.z > ROTATION_ERROR && isHitting)
+        {
+            transform.Rotate(new Vector3(0f, 0f, -0.1f) * Time.deltaTime * ROTATION_SPEED);
+        }
+        else if(transform.rotation.z - endRotation.rotation.z < ROTATION_ERROR && !isHitting)
+        {
+            transform.Rotate(new Vector3(0f, 0f, 0.1f) * Time.deltaTime * ROTATION_SPEED);
+        }
+
     }
 
     void HitUP()
     {
         movingTowards = upPosition;
+        isHitting = true;
     }
 
     void HitDown()
     {
         movingTowards = downPosition;
+        isHitting = true;
+    }
+
+    public void setHit(bool isHitting)
+    {
+        this.isHitting = isHitting;
     }
 
 }
